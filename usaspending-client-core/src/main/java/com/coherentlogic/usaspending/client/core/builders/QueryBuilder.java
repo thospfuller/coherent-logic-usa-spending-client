be55b01cbbd8b97b2c5adb697aa62563269f9063
thospfuller <thomas.fuller@coherentlogic.com>
2016-02-02 15:15:29 -0500

    Refacroting to account for changes in the Foundation Data Model.

--- usaspending-client-core/src/main/java/com/coherentlogic/usaspending/client/core/builders/QueryBuilder.java
+++ usaspending-client-core/src/main/java/com/coherentlogic/usaspending/client/core/builders/QueryBuilder.java
@@ -74,7 +74,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.client.RestTemplate;
 
-import com.coherentlogic.coherent.data.model.core.builders.rest.AbstractQueryBuilder;
+import com.coherentlogic.coherent.data.model.core.builders.rest.AbstractRESTQueryBuilder;
 import com.coherentlogic.coherent.data.model.core.cache.CacheServiceProviderSpecification;
 import com.coherentlogic.usaspending.client.core.exceptions.InvalidFormatException;
 import com.coherentlogic.usaspending.client.core.exceptions.ValueOutOfBoundsException;
@@ -99,7 +99,7 @@ import com.coherentlogic.usaspending.client.core.exceptions.ValueOutOfBoundsExce
  *
  * @author <a href="mailto:support@coherentlogic.com">Support</a>
  */
-public class QueryBuilder extends AbstractQueryBuilder {
+public class QueryBuilder extends AbstractRESTQueryBuilder {
 
     public static final String FAADS_PATH = "/faads/faads.php",
         FPDS_PATH = "/fpds/fpds.php",
